8bd06be09abec7f0eef525f3415ccaca41e4d44e
Beshoy-Ashraf <bishoy.ashraf.a046@gmail.com>
2026-09-08 19:29:56 +0300

    chore: Update .gitignore to include additional build outputs and generated files; fix property name in Booking model

--- FormulaAirLine.API/Models/Booking.cs
+++ FormulaAirLine.API/Models/Booking.cs
@@ -4,7 +4,7 @@ public class Booking
 {
       public Guid Id { get; set; }
       public string PassengerName { get; set; } = string.Empty;
-      public string PassengerSurname { get; set; } = string.Empty;
+      public string PassportNumber { get; set; } = string.Empty;
       public string PassengerEmail { get; set; } = string.Empty;
       public string PassengerPhone { get; set; } = string.Empty;
       public string From { get; set; } = string.Empty;
